a6d388795af5119cd511ef72a5f1aabda9b3d55a
wendelrios <wendelrioss@hotmail.com>
2020-06-17 15:16:11 -0300

    adding comments

--- public/js/script.js
+++ public/js/script.js
@@ -86,10 +86,10 @@ let clickedOn = false; // variavel que verifica se as estrelas estao no estado d
 
       //função responsável por adicionar classe checked de acordo com estado das estrelas 
       function starClick(i){
-        //Trecho que eu verifico se a sucessora de alguma estrela está clicada, 
-        //Exceto a última estrela, pois essa não possui sucessora
-        //Se sim, eu impeço a estrela em questão de ser clicada.
-        //Só entra nesse bloco, se a estrela tem classe checked, está clicada, não é a última e a sucessora está clicada.
+        /*Trecho que eu verifico se a sucessora de alguma estrela está clicada, 
+        exceto a última estrela, pois essa não possui sucessora
+        se sim, eu impeço a estrela em questão de ser clicada.
+        Só entra nesse bloco, se a estrela tem classe checked, está clicada, não é a última e a sucessora está clicada.*/
         if(spans[i].classList.contains('checked') && clickedOn){
           if((i<spans.length - 1)){
             if(spans[i+1].classList.contains('checked')){
